0a04ff4e4cbd19c5d09278c7a896879816443b3a
Hitesh Chopra <34310832+hiteshchopra11@users.noreply.github.com>
2022-10-30 14:17:31 +0530

    Added Custom Plugins

--- src/main/kotlin/com/example/configure/Logging.kt
+++ src/main/kotlin/com/example/configure/Logging.kt
@@ -1,10 +1,14 @@
 package com.example.configure
 
+import com.example.plugin.ErrorLoggerPlugin
+import com.example.plugin.RequestLoggerPlugin
 import io.ktor.server.application.*
 import io.ktor.server.plugins.callloging.*
 import org.slf4j.event.Level
 
 fun Application.configureLogging() {
+    install(ErrorLoggerPlugin)
+    install(RequestLoggerPlugin)
     install(CallLogging) {
         this.level = Level.INFO
     }
